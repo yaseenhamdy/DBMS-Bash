d4bf3716d22b0ef55be294ed7430f375f3f32ba5
yaseenhamdy <yaseenhamdy74@gmail.com>
2025-12-28 22:37:39 +0200

    update table menu

--- table_func/table_menu.sh
+++ table_func/table_menu.sh
@@ -24,14 +24,13 @@ tput setaf 2
         center ":.:.:.:.: Table Menu :.:.:.:.:"
         center "|                            |"
         center "|    1. Create Table         |"
-        center "|    2. Update Table Meta    |"
-        center "|    3. List Tables          |"
-        center "|    4. Drop Table           |"
-        center "|    5. Insert into Table    |"
-        center "|    6. Select from Table    |"
-        center "|    7. Update Table Data    |"
-        center "|    8. Delete from Table    |"
-        center "|    9. Back to Main Menu    |"
+        center "|    2. List Tables          |"
+        center "|    3. Drop Table           |"
+        center "|    4. Insert into Table    |"
+        center "|    5. Select from Table    |"
+        center "|    6. Update Table Data    |"
+        center "|    7. Delete from Table    |"
+        center "|    8. Back to Main Menu    |"
         center "|                            |"
         center "=============================="
         echo "" 
@@ -42,26 +41,24 @@ case $choice in
     1) create_table
        read -p "Press Enter to continue..." 
        ;;
-    2) exit
-       ;;
-    3) list_tables
+    2) list_tables
        read -p "Press Enter to continue..." 
        ;;
-    4) drop_table
+    3) drop_table
        read -p "Press Enter to continue..." 
        ;;
-    5) insert_into_table
+    4) insert_into_table
        read -p "Press Enter to continue..." 
        ;;
-   6)  select_from_table   
+    5) select_from_table   
        read -p "Press Enter to continue..."
        ;;
-   7)  update_table
+    6) update_table
        read -p "Press Enter to continue..."
        ;;
-   8) delete_from_table
+    7) delete_from_table
        ;;
-   9) main
+    8) main
        ;;
     *) center "Invalid choice, please try again."
             echo "" 
